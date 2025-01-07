eb9777a417304d53c597682d4392de75d8aa05cc
Usha Rana <usharana920@gmail.com>
2025-01-06 22:29:31 -0500

    added new classes

---
+++ src/main/java/org/usra/boa_clone/BoaCloneApplication.java
@@ -0,0 +1,13 @@
+package org.usra.boa_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoaCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoaCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/usra/boa_clone/BoaCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.usra.boa_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoaCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
